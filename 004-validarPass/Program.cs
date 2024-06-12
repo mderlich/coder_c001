24e41ec5d99ba73ea86369a45d5305a1847dd180
Matias Derlich <mderlich@gmail.com>
2024-06-12 20:43:38 -0300

    avances...

---
+++ 004-validarPass/Program.cs
@@ -0,0 +1,56 @@
+﻿
+
+bool validarContrasena(string contrasena)
+{
+    return contrasena.Equals("usSystem-2022");
+}
+
+
+string passOculto(string pass){
+    char[] caracteres = pass.ToCharArray();
+    for (int i = 0; i < caracteres.Length; i++)
+    {
+        caracteres[i] = '*';
+    }
+    string textoModificado = new string (caracteres);
+    return textoModificado;
+}
+
+void validarIngreso()
+{
+    // ---------------------------
+
+    int intentos = 0;
+    string contrasena;
+    bool ingreso = true;
+
+    Console.WriteLine("Escriba la contraseña...");
+
+
+    while (ingreso)
+    {
+        contrasena = Console.ReadLine();
+        intentos++;
+        if (validarContrasena(contrasena))
+        {
+            
+            Console.WriteLine("Bienvenido a Coderhouse...");
+            Console.WriteLine("Su contraseña es correcta {0}", passOculto(contrasena));
+            break;
+
+        }
+        else if (intentos >= 5)
+        {
+            Console.WriteLine("Contraseña incorrecta, agoto intentos...");
+            break;
+        }
+        int restoDeIntentos = 5 - intentos;
+        Console.WriteLine("Contraseña incorrecta, quedan {0} intentos...", restoDeIntentos);
+
+
+    }
+    // ---------------------------
+}
+
+
+validarIngreso();
---
+++ 004/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int sumar(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+Console.WriteLine("Ingrese numeros a sumar...");
+Console.WriteLine("**************************");
+
+Console.WriteLine("Ingrese primer numero...");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese segundo numero...");
+int num2 = int.Parse(Console.ReadLine());
+
+int total = sumar(num1, num2);
+Console.WriteLine("Total de suma... {0}", total);
