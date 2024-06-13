6e2ac3dec7ad8f79d89ab07588b3fa1233e8903d
Matias Derlich <mderlich@gmail.com>
2024-06-13 17:28:49 -0300

    avance clase C#

---
+++ 005/Producto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _005
+{
+    public class Producto
+    {
+        private int _codigo;
+        private string _descripcion;
+        private double _precioDeCompra;
+        private double _precioDeVenta;
+        private string _categoria;
+
+        public Producto(int codigo, string descripcion, double precioDeCompra, double precioDeVenta, string categoria)
+        {
+            this._codigo = codigo;
+            this._descripcion = descripcion;
+            this._precioDeCompra = precioDeCompra;
+            this._precioDeVenta = precioDeVenta;
+            this._categoria = categoria;
+        }
+        public bool hayPrecioDeVenta()
+        {
+            return this._precioDeVenta > 0;
+        }
+    }
+}
---
+++ 005/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Producto producto = new Producto(01, "pañal", 3.45, 4.5, "Limpieza");
+
+            if (producto.hayPrecioDeVenta())
+            {
+                Console.WriteLine("El producto esta a la venta");
+            }
+            else { 
+                Console.WriteLine("El producto no fue publicado aun");
+            }
+        }
+    }
+}
+
